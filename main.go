70751fc21356f1b7ade6c944594e397d2c759cd7
lsortudo <leonardo.alvirubro@hotmail.com>
2023-10-18 18:42:47 -0300

    feat: populate a fake database for example purpose

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 
 // função principal
 func main() {
+
+	// Populate the fake database ->
+	people = append(people, Person{ID: "1", Firstname: "Carlos", Lastname: "Silva", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Luiz", Lastname: "Roberto", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Andre", Lastname: "Santos"})
+	// <- Fake DB
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/contact", GetPeople).Methods("GET")
@@ -18,3 +25,21 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+func GetPeople(w http.ResponseWriter, r *http.Request)    {}
+func GetPerson(w http.ResponseWriter, r *http.Request)    {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
