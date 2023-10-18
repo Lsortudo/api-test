52c203fe2957c136ef299aa8bc5cfcb6659f227e
lsortudo <leonardo.alvirubro@hotmail.com>
2023-10-18 18:37:43 -0300

    feat: basic main methods, but empty

--- main.go
+++ main.go
@@ -10,5 +10,11 @@ import (
 // função principal
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/contact", GetPeople).Methods("GET")
+	router.HandleFunc("/contact/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/contact/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/contact/{id}", DeletePerson).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
