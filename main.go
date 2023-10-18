75ec004d19b61bbd643c60faecb3e2f87f85995d
lsortudo <leonardo.alvirubro@hotmail.com>
2023-10-18 19:03:54 -0300

    feat: Finished basic CRD

--- main.go
+++ main.go
@@ -41,10 +41,22 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 func DeletePerson(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
 
 type Person struct {
